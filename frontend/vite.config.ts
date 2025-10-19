1defd6e5c40b8b3489a0ea343ab4c548225ad2c7
Vishwa Rajapaksha <vishwarajapaksha16@gmail.com>
2025-10-20 01:27:34 +0530

    configure frontend proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Needed for the Docker Container port mapping to work
+    port: 80, // We map this port in docker-compose
+    proxy: {
+      '/api': {
+        target: 'http://api:4000', // Forward requests to our api service
+        changeOrigin: true,
+      }
+    }
+  }
 })
